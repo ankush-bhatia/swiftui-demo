89d162642db24a005f86ab37667b497a256036a1
Ankush Bhatia <ankushbhatia@Ankushs-MacBook-Pro-2.local>
2020-01-21 22:32:32 +0530

    Part 3 Starter project added.

---
+++ Part 3/Final/DemoSwiftUI/DemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSwiftUI
+//
+//  Created by Ankush Bhatia on 29/12/19.
+//  Copyright © 2019 Ankush Bhatia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
